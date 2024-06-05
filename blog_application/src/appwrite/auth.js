fe2be3ad8a0d74c360c6378ec0fb1a17cad1f4b8
inthrakumar <inthrak04@gmail.com>
2024-06-05 19:16:38 +0530

    auth service from appwrite

---
+++ blog_application/src/appwrite/auth.js
@@ -0,0 +1,53 @@
+import conf from "../conf/conf";
+
+
+import { Client, Account, ID } from "appwrite";
+
+export class Account{
+     client = new Client();
+     account;
+    constructor(){
+        this.client.getEndpoint(conf.appwriteUr).setProject(conf.appwriteProject);
+        this.account=new Account(this.client);
+
+    }
+
+    async createAccount({email,password,name}){
+        try{
+           const UserAccount= await this.account.create(ID.unique(), email, password, name);
+           if(UserAccount){
+                    this.login({email,password});
+           }else{
+            return UserAccount;
+           }
+
+        }catch{
+
+        }
+
+    }
+    
+    async login({email,password}){
+        try{
+            return this.account.createEmailPasswordSession(email,password);
+        }catch(error){
+            return error
+        }
+    }
+    async logout(){
+        try{
+            return this.account.deleteSessions();
+        }catch(error){
+            return error;
+        }
+    }
+    async getAccount(){
+        try{
+            return this.account.get();
+        }catch(error){
+            return error;
+        }
+    }
+}
+ const authservice=new Account();
+ export default authservice;
---
+++ blog_application/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf={
+    appwriteUr:String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProject:String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabase:String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollection:String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucket:String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+}
+
+export default conf;
