258e0bd0151850ae58ab00e30a2067ce64c4d81b
inthrakumar <inthrak04@gmail.com>
2024-06-05 19:41:27 +0530

    shift the blog app project

--- blog_application/src/conf/conf.js
+++
@@ -1,9 +0,0 @@
-const conf={
-    appwriteUrl:String(process.env.REACT_APP_APPWRITE_URL),
-    appwriteProject:String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
-    appwriteDatabase:String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
-    appwriteCollection:String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
-    appwriteBucket:String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
-}
-
-export default conf;
